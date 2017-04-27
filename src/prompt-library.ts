383a4e16a72ad79f548d7a62653ed1edf1905fef
Shin-Ogata <Shin.Ogata@jp.sony.com>
2017-04-27 23:07:50 +0900

    added: prompt for library

--- src/prompt-library.ts
+++ src/prompt-library.ts
@@ -3,6 +3,7 @@
 
 import * as inquirer from "inquirer";
 import * as chalk from "chalk";
+import * as semverRegex from "semver-regex";
 import {
     IProjectConfigration,
     ILibraryConfigration,
@@ -27,12 +28,87 @@ export class PromptLibrary extends PromptBase {
     get questions(): inquirer.Questions {
         // TODO:
         return [
+            // project common settnigs (IProjectConfigration)
             {
                 type: "input",
                 name: "projectName",
-                message: "TODO: プロジェクト名",
+                message: this.lang.common.projectName.message,
                 default: this.answers.projectName || "CoolProjectName",
             },
+            {
+                type: "input",
+                name: "version",
+                message: this.lang.common.version.message,
+                default: this.answers.version || "0.0.1",
+                filter: (value) => {
+                    if (semverRegex().test(value)) {
+                        return semverRegex().exec(value)[0];
+                    } else {
+                        return value;
+                    }
+                },
+                validate: (value) => {
+                    if (semverRegex().test(value)) {
+                        return true;
+                    } else {
+                        return this.lang.common.version.invalidMessage;
+                    }
+                },
+            },
+            {
+                type: "list",
+                name: "license",
+                message: this.lang.common.license.message,
+                choices: [
+                    {
+                        name: this.lang.common.license.choices.apache2,
+                        value: "Apache-2.0",
+                    },
+                    {
+                        name: this.lang.common.license.choices.mit,
+                        value: "MIT",
+                    },
+                    {
+                        name: this.lang.common.license.choices.proprietary,
+                        value: "NONE",
+                    }
+                ],
+                default: "NONE",
+            },
+            // library settnigs (ICompileConfigration)
+            {
+                type: "list",
+                name: "webpackTarget",
+                message: this.lang.library.webpackTarget.message,
+                choices: [
+                    {
+                        name: this.lang.common.webpackTarget.choices.node,
+                        value: "node",
+                    },
+                    new inquirer.Separator(),
+                    {
+                        name: this.lang.common.webpackTarget.choices.web,
+                        value: "web",
+                    },
+                    {
+                        name: this.lang.common.webpackTarget.choices.electron,
+                        value: "electron",
+                    },
+                    {
+                        name: this.lang.common.webpackTarget.choices.electronRenderer,
+                        value: "electron-renderer",
+                    }
+                ],
+                filter: (value) => {
+                    if ("node" !== value) {
+                        return "node";
+                    } else {
+                        return value;
+                    }
+                },
+                default: "node",
+            },
+            // TODO: when を使って moduleSystem
         ];
     }
 
